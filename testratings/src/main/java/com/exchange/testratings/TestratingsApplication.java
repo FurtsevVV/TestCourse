98ae3143c0140168d7fd7099f944feb8d220e467
zakat <furtsevvv@gmail.com>
2023-11-07 20:21:22 +0600

    first commit

---
+++ testratings/src/main/java/com/exchange/testratings/TestratingsApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.testratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestratingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestratingsApplication.class, args);
+	}
+
+}
